5aa01d42c41452d308759f5e4bc89904197eab65
Stanislav <lyuba.dzergun@gmail.com>
2024-10-11 10:19:01 +0400

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Какое время года вы любите? " )
+print(my_string .upper())
+print(my_string.lower())
+print(my_string .replace(" ", "#"))
+print(len(my_string))
+print(my_string[0])
+print(my_string[-1])
